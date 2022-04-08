330d03c5a80c25c08f8278f14791967a59633c33
lamech-byte <gesachmoh@gmail.com>
2022-04-08 16:38:40 +0300

    getAlbum from Ngoninit
    getAlbum from Ngoninit

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,9 +8,14 @@ import { ProductService } from 'app/product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
+  albumInfo;
+
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
   }
+
+  {}
 
 }
